4da5bf5302ce74021c6126ce050b78efde52718a
myzozoz <markusp_holopainen@yahoo.com>
2017-01-27 18:07:48 +0200

    Added Enemy and Non-Controlled-Unit (NCU) classes

---
+++ src/main/java/brimstone/game/Enemy.java
@@ -0,0 +1,8 @@
+package brimstone.game;
+
+public class Enemy extends MapObject {
+
+    public Enemy(int x, int y) {
+        super(x, y);
+    }
+}
--- src/main/java/brimstone/game/Map.java
+++ src/main/java/brimstone/game/Map.java
@@ -7,17 +7,28 @@ import java.util.ArrayList;
 public class Map {
 
     private Player plr;
+    private List<Enemy> enms;
+    private List<NCU> ncus;
 
     public Map() {
+        enms = new ArrayList();
+        ncus = new ArrayList();
         //Get the initial locations of all of the MapObjects that already exist
         //TEST
         plr = new Player(1, 2);
+        enms.add(new Enemy(2,3));
+        enms.add(new Enemy(3,4));
+        ncus.add(new NCU(4,5));
+        ncus.add(new NCU(5,6));
+        ncus.add(new NCU(6,7));
         //TEST
     }
 
     public List<MapObject> getAllObjects() {
         List<MapObject> l = new ArrayList();
         l.add(plr);
+        l.addAll(enms);
+        l.addAll(ncus);
         return l;
     }
 }
